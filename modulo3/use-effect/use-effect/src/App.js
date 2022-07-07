337340620b4ad1bd8f0a26d5debd5826327c314b
Júlia Moniz de Aragão Alves <juliamonizaa@gmail.com>
2022-07-07 13:53:32 -0300

    atividade use-effect

--- modulo3/use-effect/use-effect/src/App.js
+++ modulo3/use-effect/use-effect/src/App.js
@@ -1,11 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import PokeCard from "./Componentes/PokeCard/PokeCard";
+
+const Home = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Card = styled.div`
+  background-color: aqua;
+  border: 7px solid black;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 500px;
+`
 
 function App() {
+  const [pokeList, setPokeList] = useState([]);
+  const [pokeName, setPokeName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        setPokeList(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const changePokeName = (event) => {
+    setPokeName(event.target.value);
+  };
+  const pokeOptions = pokeList.map((pokemon) => {
+    return (
+      <option key={pokemon.name} value={pokemon.name}>
+        {pokemon.name}
+      </option>
+    );
+  });
+
+  const pokemon = pokeName && <PokeCard pokeName={pokeName} />;
+
   return (
-    <div className="App">
-      Oi
-    </div>
+    <Home>
+      <Card>
+        <label htmlFor={"select-pokemon"}>Selecione um pokemon: </label>
+        <select id={"select-pokemon"} onChange={changePokeName}>
+          <option value={""}>Nenhum</option>
+          {pokeOptions}
+        </select>
+        {pokemon}
+      </Card>
+    </Home>
   );
 }
 
