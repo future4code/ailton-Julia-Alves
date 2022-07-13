3657e49e3ee3ef88c5a85077ac36f80c86ebb453
Júlia Moniz de Aragão Alves <juliamonizaa@gmail.com>
2022-07-13 20:40:35 -0300

    Primeira api funcionando

--- modulo3/projeto-labex/projeto-labex/src/Pages/Home/Home.js
+++ modulo3/projeto-labex/projeto-labex/src/Pages/Home/Home.js
@@ -9,12 +9,29 @@ const HomeContainer = styled.div`
   background-repeat: no-repeat;
   height: 90vh;
 `
+const HomeDiv = styled.div`
+  background-color: #0095B7;
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  height: 30vh;
+  margin-left: 150px;
+  opacity: 0.4;
+  border-radius: 9px;
+  box-shadow: 0 0 12vh #33FFFF
+`
 export const Home = ()=>{
   return(
     <div> 
       <Header/>
       <HomeContainer>
+        <br/>
+        <br/>
+        <br/>
+        <HomeDiv>
+        <h1>Sobre nós</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ex at nulla voluptatum quidem quis unde quasi nostrum, sed iste repellat omnis nihil minima. Architecto autem quas exercitationem? Ipsa, quisquam.</p>
+        </HomeDiv>
       </HomeContainer>
     </div>
   )
--- modulo3/projeto-labex/projeto-labex/src/Pages/ListTrip/ListTrip.js
+++ modulo3/projeto-labex/projeto-labex/src/Pages/ListTrip/ListTrip.js
@@ -11,6 +11,25 @@ const ListTripContainer = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const DIVQUALQUER = styled.div`
+  background: linear-gradient(to right, 	#47ABCC, #02A4D3, 	#0095B7);
+  box-shadow: rgb(0 0 0 / 30%) 0px 4px 8px 0px;
+    padding: 10px 20px;
+    border-radius: 4px;
+    margin: 50px 0px;
+    width: 60%;
+    opacity: 0.8;
+`
+const DIVQUALQUER2 = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  width: 70%;
 `
 export const ListTrip = ()=>{
 const [trips,setTrips]=useState()
@@ -25,17 +44,28 @@ useEffect(()=>{
 const getTrips=()=>{
   const url = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/julia-moniz-ailton/trips"
   axios.get(url)
-  .then((res)=>console.log(res))
+  .then((res)=>{
+    console.log(res)
+    setTrips(res.data.trips)
+  })
   .catch((err)=>console.log(err))
 }
   return(
     <div>
       <Header/>
-      <ListTrip>
-      eu sou a list trip
-      <br/>
-      <button onClick={goToApplication}>Application</button>
-      </ListTrip>
+      <ListTripContainer>
+      <DIVQUALQUER2>
+        {trips?.map((dado)=>{
+          return <DIVQUALQUER key={dado.id}>
+          <p>{dado.name}</p>
+          <p>{dado.descrition}</p>
+          <p>{dado.planet}</p>
+          <p>{dado.date},{dado.durationInDays}</p>
+          <button onClick={goToApplication}> Quero me candidatar</button>
+          </DIVQUALQUER>
+        })}
+      </DIVQUALQUER2>
+      </ListTripContainer>
     </div>
   );
 };
